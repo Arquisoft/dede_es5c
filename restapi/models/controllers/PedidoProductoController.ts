cd38f1957c2dc48b51f452b9e6707518d7a3871e
uo277516 <uo277516@uniovi.es>
2022-04-05 23:40:08 +0200

    añadido el modelo y esquema de la tabla ProductoPedido

---
+++ restapi/models/controllers/PedidoProductoController.ts
@@ -0,0 +1,24 @@
+require("../db/db")
+import { Request, Response } from 'express';
+import ProductoPedido from '../PedidoProductoModel';
+
+export const findAllOrderProducts = async (req: Request, res: Response): Promise<Response> => {
+    const orderProducts = await ProductoPedido.find({});
+
+    return res.json(orderProducts);
+};
+
+export const addOrderProduct = async (req: Request, res: Response): Promise<Response> => {
+
+    const orderProductReq = req.body
+
+    const nOrderProduct =  new ProductoPedido({
+        quantity: orderProductReq.quantity,
+        id_product: orderProductReq.id_product,
+        id_order: orderProductReq.id_order
+    })
+
+    nOrderProduct.save()
+
+    return res.status(200).json({ nOrderProduct });
+};
