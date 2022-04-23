1b50c8504a9b475ecc9c5b9fa6dbb81402525ec3
xurde <uo271033@uniovi.es>
2022-04-23 20:50:18 +0200

    fallo api

--- webapp/src/api/api.ts
+++ webapp/src/api/api.ts
@@ -1,5 +1,5 @@
 
-import {User, Product} from '../shared/shareddtypes';
+import {User, Product, Pedido} from '../shared/shareddtypes';
 
 
 export async function addUser(user:User):Promise<boolean>{
