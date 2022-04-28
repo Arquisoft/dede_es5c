ac4e7e9bf5dfd8e750ab43ec3478de07d7cb43ab
lucas <UO277440@uniovi.es>
2022-04-28 19:54:12 +0200

    metodo para calcular las coordenadas de una dirección dada

--- restapi/controllers/PedidoController.ts
+++ restapi/controllers/PedidoController.ts
@@ -1,13 +1,20 @@
 //consultas
 
 import ProductModel from "../models/ProductModel";
-
+import { Request, Response } from "express";
 require("../database")
+import { Request, Response } from "express";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 
 import {Request, Response} from "express";
 import api from "../api";
 import PedidoModel from "../models/PedidoModel";
+import * as url from "url";
+const TOKEN:string = "pk.eyJ1IjoidW8yNzc0NDAiLCJhIjoiY2wyaXBhaGZkMDc4YjNqcW5qenY5MjFvOCJ9.0oTGSdJTHf7bwxxiK9jCKg";
 
 export const findPedidos = async (req: Request, res: Response): Promise<Response> => {
     const p = await PedidoModel.find({})
@@ -36,3 +43,29 @@ export const findPedidoByWebid = async (req: Request, res: Response): Promise<Re
     return res.json(p);
 
 };
+
+async function calcularCoordenadas(direccion: { zipcode: any; country: any; number: any; city: any; street: any }) {
+    var uri = new URL('https://api.mapbox.com/geocoding/v5/mapbox.places/' + direccion.number + '%20' + direccion.street + '%20' + direccion.city + '%20' + direccion.country + '%20' + direccion.zipcode + '.json?access_token=' + TOKEN);
+    return await fetch(uri.toString())
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (coordenadas) {
+            return {
+                "long": coordenadas.features[0].center[0],
+                "lat": coordenadas.features[0].center[1],
+            }
+        });
+}
+
+export const calculatePrice = async (req: Request, res: Response): Promise<Response> => {
+    let direccion = {
+        "zipcode": req.body.zipcode,
+        "city": req.body.city,
+        "street": req.body.street,
+        "country": req.body.country,
+        "number": req.body.number
+    }
+    var coordenadasCliente = calcularCoordenadas (direccion);
+
+}
